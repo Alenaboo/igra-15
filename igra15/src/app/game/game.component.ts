25d2d130d220041eae4cddbbab8bee138a716ccf
Алёна Горбачёва <alenagorbaceva@Alenas-MacBook-Pro.local>
2021-05-29 19:57:44 +0300

    Picture mode & nicer style

--- igra15/src/app/game/game.component.ts
+++ igra15/src/app/game/game.component.ts
@@ -9,14 +9,14 @@ import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 export class GameComponent implements OnInit {
   @ViewChild('victoryTemplate')
   private victoryTemplate: TemplateRef<any> | undefined;
-  private readonly ROW_SIZE = 4;
   public modalRef: BsModalRef | undefined;
-  public cellList: (number | null)[] = [
+  public cellList: number[] = [
     1, 2, 3, 4,
     5, 6, 7, 8,
     9, 10, 11, 12,
-    13, 14, 15, null
+    13, 14, 15, 16
   ];
+  private readonly ROW_SIZE = Math.sqrt(this.cellList.length);
 
 
   constructor(
@@ -33,10 +33,10 @@ export class GameComponent implements OnInit {
   public moveCell(cellId: number) {
     const newCellId = this.canMoveCell(cellId);
     if (newCellId >= 0) {
-      // copying value to new position
+      // swapping values
+      const tmpValue = this.cellList[newCellId];
       this.cellList[newCellId] = this.cellList[cellId];
-      // cleaning previous position
-      this.cellList[cellId] = null;
+      this.cellList[cellId] = tmpValue;
 
       // Check if the game is complete
       if (this.isMidvymove()) {
@@ -48,35 +48,41 @@ export class GameComponent implements OnInit {
   public canMoveCell(cellId: number): number {
     // cellId = 5
     // cellId up = 1
-    if (cellId - this.ROW_SIZE >= 0 && this.cellList[cellId - this.ROW_SIZE] === null) {
+    if (cellId - this.ROW_SIZE >= 0 && this.cellList[cellId - this.ROW_SIZE] === this.cellList.length) {
       return cellId - this.ROW_SIZE;
     }
     // cellId down = 9
-    if (cellId + this.ROW_SIZE < this.ROW_SIZE**2 && this.cellList[cellId + this.ROW_SIZE] === null) {
+    if (cellId + this.ROW_SIZE < this.ROW_SIZE**2 && this.cellList[cellId + this.ROW_SIZE] === this.cellList.length) {
       return cellId + this.ROW_SIZE;
     }
     // cellId left = 4
-    if (Math.ceil((cellId + 1)/4) === Math.ceil(cellId/4) && this.cellList[cellId - 1] === null) {
+    if (Math.ceil((cellId + 1)/4) === Math.ceil(cellId/4) && this.cellList[cellId - 1] === this.cellList.length) {
       return cellId - 1;
     }
     // cellId right = 6
-    if (Math.ceil((cellId + 1)/4) === Math.ceil((cellId + 2)/4) && this.cellList[cellId + 1] === null) {
+    if (Math.ceil((cellId + 1)/4) === Math.ceil((cellId + 2)/4) && this.cellList[cellId + 1] === this.cellList.length) {
       return cellId + 1;
     }
     return -1;
+  }
+
+  public bgWidth(cellValue: number): number {
+    return (cellValue - 1) % this.ROW_SIZE * 100 / (this.ROW_SIZE - 1);
+  }
+
+  public bgHeight(cellValue: number): number {
+    return (Math.ceil(cellValue / this.ROW_SIZE) - 1) * 100 / (this.ROW_SIZE - 1);
   }
 
   /**
    * @description Check if the game is complete / the last move was the winning one
    */
   private isMidvymove(): boolean {
-    return this.cellList.every(( cellValue, cellIdx, cellList ) => 
-      cellValue === (cellList.length === cellIdx + 1 ? null : cellIdx + 1)
-    );
+    return this.cellList.every(( cellValue, cellIdx ) => cellValue === cellIdx + 1);
   }
 
   ngOnInit(): void {
-    this.shuffle();
+    //this.shuffle();
   }
 
   private config = {
